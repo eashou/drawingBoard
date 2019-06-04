cdbac8f8437fe3dd8b994d25005664dfaf3dd6a9
shaoyishou <shaoyishou@jd.com>
2019-06-04 18:51:39 +0800

    simplification code

--- dist/drawing-board.js
+++ dist/drawing-board.js
@@ -1,10 +1,38 @@
 'use strict';
 var DrawingBoard = /** @class */ (function () {
     function DrawingBoard(el) {
+        var _this = this;
+        this.isDrawing = false;
+        this.shapeList = [];
         this.ctx = el.getContext('2d');
+        el.addEventListener('mousedown', function (ev) {
+            _this.drawStart(ev.offsetX, ev.offsetY);
+        }, false);
+        el.addEventListener('mousemove', function (ev) {
+            _this.drawing(ev.offsetX, ev.offsetY);
+        }, false);
+        el.addEventListener('mouseup', this.drawEnd.bind(this), false);
     }
-    DrawingBoard.prototype.getCtx = function () {
-        return this.ctx;
+    DrawingBoard.prototype.drawStart = function (x, y) {
+        this.isDrawing = true;
+        this.shapeList.push([x, y]);
+    };
+    DrawingBoard.prototype.drawing = function (x, y) {
+        var _this = this;
+        if (this.isDrawing) {
+            this.shapeList.push([x, y]);
+            this.ctx.beginPath();
+            this.shapeList.forEach(function (shape) {
+                _this.ctx.lineTo(shape[0], shape[1]);
+                _this.ctx.moveTo(shape[0], shape[1]);
+            });
+            this.ctx.closePath();
+            this.ctx.stroke();
+        }
+    };
+    DrawingBoard.prototype.drawEnd = function () {
+        this.isDrawing = false;
+        this.shapeList = [];
     };
     return DrawingBoard;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
 'use strict';
 
 class DrawingBoard {
-  ctx: object;
+  ctx: CanvasRenderingContext2D;
+  isDrawing: boolean = false;
+  shapeList: Array<number[]> = [];
 
   constructor(el: HTMLCanvasElement) {
     this.ctx = el.getContext('2d');
+
+
+    el.addEventListener('mousedown', ev => {
+      this.drawStart(ev.offsetX, ev.offsetY)
+    }, false)
+    el.addEventListener('mousemove', ev => {
+      this.drawing(ev.offsetX, ev.offsetY)
+    }, false);
+    el.addEventListener('mouseup', this.drawEnd.bind(this), false);
   }
 
-  getCtx() {
-    return this.ctx
+  drawStart (x: number, y: number) {
+    this.isDrawing = true;
+    this.shapeList.push([x, y]);
+  }
+
+  drawing (x: number, y: number) {
+    if (this.isDrawing) {
+      this.shapeList.push([x, y]);
+      this.ctx.beginPath();
+      this.shapeList.forEach((shape: number[]) => {
+        this.ctx.lineTo(shape[0], shape[1]);
+        this.ctx.moveTo(shape[0], shape[1]);
+      });
+      this.ctx.closePath();
+      this.ctx.stroke();
+    }
+  }
+
+  drawEnd () {
+    this.isDrawing = false;
+    this.shapeList = [];
   }
 }
