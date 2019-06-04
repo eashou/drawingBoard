ef5943f52e1ebf063f25082ec5a8d1f871c90ee8
shaoyishou <shaoyishou@jd.com>
2019-06-04 16:05:42 +0800

    canvas add getContext

--- dist/drawing-board.js
+++ dist/drawing-board.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+'use strict';
+var DrawingBoard = /** @class */ (function () {
+    function DrawingBoard(el) {
+        this.ctx = el.getContext('2d');
+    }
+    DrawingBoard.prototype.getCtx = function () {
+        return this.ctx;
+    };
+    return DrawingBoard;
+}());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+'use strict';
+
+class DrawingBoard {
+  ctx: object;
+
+  constructor(el: HTMLCanvasElement) {
+    this.ctx = el.getContext('2d');
+  }
+
+  getCtx() {
+    return this.ctx
+  }
+}
