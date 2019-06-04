21b06887d95b6686356d6b1466f7655d19fb48f3
shaoyishou <shaoyishou@jd.com>
2019-06-04 21:25:21 +0800

    simplification text code

--- dist/drawing-board.js
+++ dist/drawing-board.js
@@ -7,20 +7,32 @@ var DrawingBoard = /** @class */ (function () {
     function DrawingBoard(el, options) {
         var _this = this;
         if (options === void 0) { options = defaultArgs; }
+        this.el = null;
         this.isDrawing = false;
         this.shapeList = [];
         this.currentTool = 'pan';
+        this.txtInput = null;
+        this.el = el;
         this.ctx = el.getContext('2d');
         this.currentTool = options.currentTool;
-        if (this.currentTool === 'pan') {
-            el.addEventListener('mousedown', function (ev) {
+        el.addEventListener('mousedown', function (ev) {
+            if (_this.currentTool === 'pan') {
                 _this.drawStart(ev.offsetX, ev.offsetY);
-            }, false);
-            el.addEventListener('mousemove', function (ev) {
-                _this.drawing(ev.offsetX, ev.offsetY);
-            }, false);
-            el.addEventListener('mouseup', this.drawEnd.bind(this), false);
-        }
+            }
+            else if (_this.currentTool === 'text') {
+                _this.text(ev.offsetX, ev.offsetY);
+            }
+        }, false);
+        el.addEventListener('mousemove', function (ev) {
+            _this.drawing(ev.offsetX, ev.offsetY);
+        }, false);
+        el.addEventListener('mouseup', function () {
+            if (_this.currentTool === 'pan') {
+                _this.drawEnd();
+            }
+            else if (_this.currentTool === 'text') {
+            }
+        }, false);
     }
     DrawingBoard.prototype.drawStart = function (x, y) {
         this.isDrawing = true;
@@ -42,6 +54,23 @@ var DrawingBoard = /** @class */ (function () {
     DrawingBoard.prototype.drawEnd = function () {
         this.isDrawing = false;
         this.shapeList = [];
+    };
+    DrawingBoard.prototype.text = function (x, y) {
+        if (this.txtInput) {
+            this.ctx.fillText(this.txtInput.value, this.shapeList[0][0], this.shapeList[0][1]);
+            this.el.parentNode.removeChild(this.txtInput);
+            this.txtInput = null;
+            this.shapeList = [];
+        }
+        else {
+            this.shapeList.push([x, y]);
+            this.txtInput = document.createElement('textarea');
+            this.txtInput.style.position = 'absolute';
+            this.txtInput.style.left = x + 'px';
+            this.txtInput.style.top = y + 'px';
+            this.txtInput.style.border = '1px solid #000';
+            this.el.parentNode.appendChild(this.txtInput);
+        }
     };
     return DrawingBoard;
 }());
--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,35 @@ const defaultArgs: ConfigArgs = {
 };
 
 class DrawingBoard {
+  el: HTMLCanvasElement = null;
   ctx: CanvasRenderingContext2D;
   isDrawing: boolean = false;
   shapeList: Array<number[]> = [];
   currentTool: string = 'pan';
+  txtInput: HTMLTextAreaElement = null;
 
   constructor(el: HTMLCanvasElement, options: ConfigArgs = defaultArgs) {
+    this.el = el;
     this.ctx = el.getContext('2d');
     this.currentTool = options.currentTool;
 
-    if (this.currentTool === 'pan') {
-      el.addEventListener('mousedown', ev => {
+    el.addEventListener('mousedown', ev => {
+      if (this.currentTool === 'pan') {
         this.drawStart(ev.offsetX, ev.offsetY);
-      }, false);
-      el.addEventListener('mousemove', ev => {
-        this.drawing(ev.offsetX, ev.offsetY)
-      }, false);
-      el.addEventListener('mouseup', this.drawEnd.bind(this), false);
-    }
+      } else if (this.currentTool === 'text') {
+        this.text(ev.offsetX, ev.offsetY);
+      }
+    }, false);
+    el.addEventListener('mousemove', ev => {
+      this.drawing(ev.offsetX, ev.offsetY)
+    }, false);
+    el.addEventListener('mouseup', () => {
+      if (this.currentTool === 'pan') {
+        this.drawEnd();
+      } else if (this.currentTool === 'text') {
+
+      }
+    }, false);
   }
 
   drawStart (x: number, y: number) {
@@ -50,5 +61,22 @@ class DrawingBoard {
   drawEnd () {
     this.isDrawing = false;
     this.shapeList = [];
+  }
+
+  text (x: number, y: number) {
+    if (this.txtInput) {
+      this.ctx.fillText(this.txtInput.value, this.shapeList[0][0], this.shapeList[0][1]);
+      this.el.parentNode.removeChild(this.txtInput);
+      this.txtInput = null;
+      this.shapeList = [];
+    } else {
+      this.shapeList.push([x, y]);
+      this.txtInput = document.createElement('textarea');
+      this.txtInput.style.position = 'absolute';
+      this.txtInput.style.left = x + 'px';
+      this.txtInput.style.top = y + 'px';
+      this.txtInput.style.border = '1px solid #000';
+      this.el.parentNode.appendChild(this.txtInput);
+    }
   }
 }
