34d97c71c5d9c5d164d9c27c7bdf6351f2ef0ab2
shaoyishou <shaoyishou@gmail.com>
2019-06-13 17:05:31 +0800

    some tool classes are encapsulated

--- dist/drawing-board.js
+++ dist/drawing-board.js
@@ -1,105 +1,202 @@
 'use strict';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Point = /** @class */ (function () {
+    function Point(x, y, size, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.color = color;
+    }
+    return Point;
+}());
+var Line = /** @class */ (function () {
+    function Line(ctx, start, end) {
+        this.ctx = ctx;
+        this.start = start;
+        this.end = end;
+        this.draw();
+    }
+    Line.prototype.draw = function () {
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.start.x, this.start.y);
+        this.ctx.lineTo(this.end.x, this.end.y);
+        this.ctx.lineWidth = this.start.size;
+        this.ctx.strokeStyle = this.start.color;
+        this.ctx.stroke();
+        this.ctx.save();
+    };
+    return Line;
+}());
+var Pencil = /** @class */ (function () {
+    function Pencil(ctx, PointList) {
+        this.ctx = ctx;
+        this.PointList = PointList;
+        this.draw();
+    }
+    Pencil.prototype.draw = function () {
+        var _this_1 = this;
+        this.PointList.forEach(function (point, index) {
+            var lastPoint = _this_1.PointList[index - 1];
+            lastPoint && new Line(_this_1.ctx, lastPoint, point);
+        });
+    };
+    return Pencil;
+}());
+var Eraser = /** @class */ (function (_super) {
+    __extends(Eraser, _super);
+    function Eraser(ctx, PointList) {
+        var _this_1 = _super.call(this, ctx, PointList) || this;
+        _this_1.draw();
+        return _this_1;
+    }
+    Eraser.prototype.draw = function () {
+        this.ctx.globalCompositeOperation = 'destination-out';
+        _super.prototype.draw.call(this);
+        this.ctx.globalCompositeOperation = 'source-over';
+    };
+    return Eraser;
+}(Pencil));
+var Rect = /** @class */ (function () {
+    function Rect(ctx, start, width, height, bgColor) {
+        this.ctx = ctx;
+        this.start = start;
+        this.width = width;
+        this.height = height;
+        this.bgColor = bgColor;
+        this.draw();
+    }
+    Rect.prototype.draw = function () {
+        this.ctx.lineWidth = this.start.size;
+        this.ctx.strokeStyle = this.start.color;
+        if (this.bgColor) {
+            this.ctx.fillStyle = this.bgColor;
+            this.ctx.fillRect(this.start.x, this.start.y, this.width, this.height);
+        }
+        this.ctx.strokeRect(this.start.x, this.start.y, this.width, this.height);
+    };
+    return Rect;
+}());
+var Txt = /** @class */ (function () {
+    function Txt(ctx, start, text) {
+        this.ctx = ctx;
+        this.start = start;
+        this.text = text;
+        this.draw();
+    }
+    Txt.prototype.draw = function () {
+        this.ctx.fillText(this.text, this.start.x, this.start.y);
+    };
+    return Txt;
+}());
 ;
 var defaultArgs = {
-    currentTool: 'pen'
+    currentTool: 'pencil'
 };
 var DrawingBoard = /** @class */ (function () {
     function DrawingBoard(el, options) {
-        var _this = this;
+        var _this_1 = this;
         if (options === void 0) { options = defaultArgs; }
         this.el = null;
-        this.isDrawing = false;
-        this.shapeList = [];
-        this.currentTool = 'pen';
         this.txtInput = null;
+        this.isDrawing = false;
+        this.pointList = [];
+        this.shapeList = [];
+        this.currentTool = 'pencil';
         this.el = el;
         this.ctx = el.getContext('2d');
         this.currentTool = options.currentTool;
+        this.init();
         el.addEventListener('mousedown', function (ev) {
-            if (_this.currentTool === 'text') {
-                _this.text(ev.offsetX, ev.offsetY);
-            }
-            else {
-                _this.drawStart(ev.offsetX, ev.offsetY);
-            }
+            _this_1.drawStart(ev.offsetX, ev.offsetY);
         }, false);
         el.addEventListener('mousemove', function (ev) {
-            if (_this.currentTool === 'pen' || _this.currentTool === 'eraser') {
-                _this.drawing(ev.offsetX, ev.offsetY);
-            }
-            else if (_this.currentTool === 'rect') {
-                _this.rect(ev.offsetX, ev.offsetY);
-            }
-            else if (_this.currentTool === 'line') {
-                _this.line(ev.offsetX, ev.offsetY);
-            }
+            _this_1.drawing(ev.offsetX, ev.offsetY);
         }, false);
-        document.addEventListener('mouseup', function () {
-            if (_this.currentTool !== 'text') {
-                _this.drawEnd();
-            }
+        document.addEventListener('mouseup', function (ev) {
+            _this_1.drawEnd(ev.offsetX, ev.offsetY);
         }, false);
     }
-    DrawingBoard.prototype.clear = function () {
+    DrawingBoard.prototype.generatePoint = function (x, y, size, color) {
+        size = size || 5;
+        color = color || 'red';
+        return new Point(x, y, size, color);
+    };
+    DrawingBoard.prototype.createTxtInput = function (start) {
+        if (this.txtInput)
+            return;
+        this.txtInput = document.createElement('textarea');
+        this.txtInput.style.position = 'absolute';
+        this.txtInput.style.left = start.x + 'px';
+        this.txtInput.style.top = start.y + 'px';
+        this.txtInput.style.border = '1px solid #000';
+        this.el.parentNode.appendChild(this.txtInput);
+        this.txtInput.focus();
+        var _this = this;
+        var timer = setTimeout(function () {
+            function onBlur() {
+                var text = _this.txtInput.value;
+                _this.el.parentNode.removeChild(_this.txtInput);
+                _this.txtInput.removeEventListener('blur', onBlur, false);
+                _this.txtInput = null;
+                if (text) {
+                    _this.shapeList.push(new Txt(_this.ctx, start, text));
+                }
+            }
+            _this.txtInput.addEventListener('blur', onBlur, false);
+            clearTimeout(timer);
+        }, 30);
+    };
+    DrawingBoard.prototype.init = function () {
         this.ctx.clearRect(0, 0, this.el.width, this.el.height);
+        this.shapeList.forEach(function (shape) {
+            shape.draw();
+        });
     };
     DrawingBoard.prototype.drawStart = function (x, y) {
         this.isDrawing = true;
-        this.shapeList.push([x, y]);
+        this.pointList.push(this.generatePoint(x, y));
+        if (this.currentTool === 'text') {
+            this.createTxtInput(this.pointList[this.pointList.length - 1]);
+        }
     };
     DrawingBoard.prototype.drawing = function (x, y) {
-        var _this = this;
-        if (this.isDrawing) {
-            this.shapeList.push([x, y]);
-            this.ctx.beginPath();
-            if (this.currentTool === 'eraser') {
-                this.ctx.globalCompositeOperation = 'destination-out';
-            }
-            this.shapeList.forEach(function (shape) {
-                _this.ctx.lineTo(shape[0], shape[1]);
-                _this.ctx.moveTo(shape[0], shape[1]);
-            });
-            this.ctx.closePath();
-            this.ctx.stroke();
+        if (!this.isDrawing)
+            return;
+        this.init();
+        var start = this.pointList[0];
+        var end = this.generatePoint(x, y);
+        this.pointList.push(end);
+        if (this.currentTool === 'pencil') {
+            this.shape = new Pencil(this.ctx, this.pointList);
+        }
+        else if (this.currentTool === 'eraser') {
+            this.shape = new Eraser(this.ctx, this.pointList);
+        }
+        else if (this.currentTool === 'line') {
+            this.shape = new Line(this.ctx, start, this.generatePoint(x, y));
+        }
+        else if (this.currentTool === 'rect') {
+            this.shape = new Rect(this.ctx, start, x - start.x, y - start.y);
         }
     };
-    DrawingBoard.prototype.drawEnd = function () {
+    DrawingBoard.prototype.drawEnd = function (x, y) {
+        this.isDrawing && this.shape && this.shapeList.push(this.shape);
+        this.pointList = [];
         this.isDrawing = false;
-        this.shapeList = [];
-    };
-    DrawingBoard.prototype.text = function (x, y) {
-        if (this.txtInput) {
-            this.ctx.fillText(this.txtInput.value, this.shapeList[0][0], this.shapeList[0][1]);
-            this.el.parentNode.removeChild(this.txtInput);
-            this.txtInput = null;
-            this.shapeList = [];
-        }
-        else {
-            this.shapeList.push([x, y]);
-            this.txtInput = document.createElement('textarea');
-            this.txtInput.style.position = 'absolute';
-            this.txtInput.style.left = x + 'px';
-            this.txtInput.style.top = y + 'px';
-            this.txtInput.style.border = '1px solid #000';
-            this.el.parentNode.appendChild(this.txtInput);
-        }
-    };
-    DrawingBoard.prototype.rect = function (x, y) {
-        if (this.isDrawing) {
-            this.clear();
-            var firstShape = this.shapeList[0];
-            this.shapeList.push([x, y]);
-            this.ctx.fillRect(firstShape[0], firstShape[1], x - firstShape[0], y - firstShape[1]);
-        }
-    };
-    DrawingBoard.prototype.line = function (x, y) {
-        if (this.isDrawing) {
-            this.clear();
-            this.ctx.beginPath();
-            this.ctx.moveTo(this.shapeList[0][0], this.shapeList[0][1]);
-            this.ctx.lineTo(x, y);
-            this.ctx.stroke();
-        }
+        this.shape = null;
+        console.log(this.shapeList);
     };
     return DrawingBoard;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,112 +1,222 @@
 'use strict';
 
+class Point {
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+
+  constructor (x: number, y: number, size: number, color: string) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+}
+
+class Line {
+  ctx: CanvasRenderingContext2D;
+  start: Point;
+  end: Point;
+
+  constructor (ctx: CanvasRenderingContext2D, start: Point, end: Point) {
+    this.ctx = ctx;
+    this.start = start;
+    this.end = end;
+    this.draw();
+  }
+
+  draw () {
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.start.x, this.start.y);
+    this.ctx.lineTo(this.end.x, this.end.y);
+    this.ctx.lineWidth = this.start.size;
+    this.ctx.strokeStyle = this.start.color;
+    this.ctx.stroke();
+    this.ctx.save();
+  }
+}
+
+class Pencil {
+  ctx: CanvasRenderingContext2D;
+  PointList: Array<Point>;
+
+  constructor (ctx: CanvasRenderingContext2D, PointList: Array<Point>) {
+    this.ctx = ctx;
+    this.PointList = PointList;
+    this.draw()
+  }
+
+  draw () {
+    this.PointList.forEach((point, index) => {
+      const lastPoint = this.PointList[index - 1];
+      lastPoint && new Line(this.ctx, lastPoint, point);
+    })
+  }
+}
+
+class Eraser extends Pencil {
+  constructor (ctx: CanvasRenderingContext2D, PointList: Array<Point>) {
+    super(ctx, PointList);
+    this.draw();
+  }
+
+  draw () {
+    this.ctx.globalCompositeOperation = 'destination-out';
+    super.draw();
+    this.ctx.globalCompositeOperation = 'source-over';
+  }
+}
+
+class Rect {
+  ctx: CanvasRenderingContext2D;
+  start: Point;
+  width: number;
+  height: number;
+  bgColor: string;
+
+  constructor (ctx: CanvasRenderingContext2D, start: Point, width: number, height: number, bgColor?: string) {
+    this.ctx = ctx;
+    this.start = start;
+    this.width = width;
+    this.height = height;
+    this.bgColor = bgColor;
+    this.draw();
+  }
+
+  draw () {
+    this.ctx.lineWidth = this.start.size;
+    this.ctx.strokeStyle = this.start.color;
+    if (this.bgColor) {
+      this.ctx.fillStyle = this.bgColor;
+      this.ctx.fillRect(this.start.x, this.start.y, this.width, this.height);
+    }
+    this.ctx.strokeRect(this.start.x, this.start.y, this.width, this.height);
+  }
+}
+
+class Txt {
+  ctx: CanvasRenderingContext2D;
+  start: Point;
+  text: string;
+
+  constructor (ctx: CanvasRenderingContext2D, start: Point, text: string) {
+    this.ctx = ctx;
+    this.start = start;
+    this.text = text;
+    this.draw();
+  }
+
+  draw () {
+    this.ctx.fillText(this.text, this.start.x, this.start.y);
+  }
+}
+
+
 interface ConfigArgs {
   currentTool: string
 };
 
 const defaultArgs: ConfigArgs = {
-  currentTool: 'pen'
+  currentTool: 'pencil'
 };
 
 class DrawingBoard {
   el: HTMLCanvasElement = null;
+  txtInput: HTMLTextAreaElement = null;
   ctx: CanvasRenderingContext2D;
   isDrawing: boolean = false;
-  shapeList: Array<number[]> = [];
-  currentTool: string = 'pen';
-  txtInput: HTMLTextAreaElement = null;
+  pointList: Array<Point> = [];
+  shape: Line|Pencil|Eraser|Rect|Txt;
+  shapeList: Array<Line|Pencil|Eraser|Rect|Txt> = [];
+  currentTool: string = 'pencil';
 
   constructor(el: HTMLCanvasElement, options: ConfigArgs = defaultArgs) {
     this.el = el;
     this.ctx = el.getContext('2d');
     this.currentTool = options.currentTool;
+    this.init();
 
     el.addEventListener('mousedown', ev => {
-      if (this.currentTool === 'text') {
-        this.text(ev.offsetX, ev.offsetY);
-      } else {
-        this.drawStart(ev.offsetX, ev.offsetY);
-      }
+      this.drawStart(ev.offsetX, ev.offsetY);
     }, false);
     el.addEventListener('mousemove', ev => {
-      if (this.currentTool === 'pen' || this.currentTool === 'eraser') {
-        this.drawing(ev.offsetX, ev.offsetY);
-      } else if (this.currentTool === 'rect') {
-        this.rect(ev.offsetX, ev.offsetY);
-      } else if (this.currentTool === 'line') {
-        this.line(ev.offsetX, ev.offsetY);
-      }
+      this.drawing(ev.offsetX, ev.offsetY);
     }, false);
-    document.addEventListener('mouseup', () => {
-      if (this.currentTool !== 'text') {
-        this.drawEnd();
-      }
+    document.addEventListener('mouseup', ev => {
+      this.drawEnd(ev.offsetX, ev.offsetY);
     }, false);
   }
 
-  clear () {
+  generatePoint (x: number, y: number, size?: number, color?: string) {
+    size = size || 5
+    color = color || 'red'
+    return new Point(x, y, size, color)
+  }
+
+  createTxtInput (start: Point) {
+    if (this.txtInput) return
+    this.txtInput = document.createElement('textarea');
+    this.txtInput.style.position = 'absolute';
+    this.txtInput.style.left = start.x + 'px';
+    this.txtInput.style.top = start.y + 'px';
+    this.txtInput.style.border = '1px solid #000';
+    this.el.parentNode.appendChild(this.txtInput);
+    this.txtInput.focus();
+
+    const _this = this;
+    const timer = setTimeout(() => {
+      function onBlur () {
+        const text = _this.txtInput.value;
+        _this.el.parentNode.removeChild(_this.txtInput);
+        _this.txtInput.removeEventListener('blur', onBlur, false);
+        _this.txtInput = null;
+        if (text) {
+          _this.shapeList.push(new Txt(_this.ctx, start, text));
+        }
+      }
+      _this.txtInput.addEventListener('blur', onBlur, false);
+      clearTimeout(timer);
+    }, 30)
+  }
+
+  init () {
     this.ctx.clearRect(0, 0, this.el.width, this.el.height);
+    this.shapeList.forEach(shape => {
+      shape.draw();
+    });
   }
 
   drawStart (x: number, y: number) {
     this.isDrawing = true;
-    this.shapeList.push([x, y]);
+    this.pointList.push(this.generatePoint(x, y));
+    if (this.currentTool === 'text') {
+      this.createTxtInput(this.pointList[this.pointList.length - 1])
+    }
   }
 
   drawing (x: number, y: number) {
-    if (this.isDrawing) {
-      this.shapeList.push([x, y]);
-      this.ctx.beginPath();
-      if (this.currentTool === 'eraser') {
-        this.ctx.globalCompositeOperation = 'destination-out'
-      }
-      this.shapeList.forEach((shape: number[]) => {
-        this.ctx.lineTo(shape[0], shape[1]);
-        this.ctx.moveTo(shape[0], shape[1]);
-      });
-      this.ctx.closePath();
-      this.ctx.stroke();
+    if (!this.isDrawing) return
+    this.init();
+    const start: Point = this.pointList[0];
+    const end: Point = this.generatePoint(x, y);
+    this.pointList.push(end);
+    if (this.currentTool === 'pencil') {
+      this.shape = new Pencil(this.ctx, this.pointList)
+    } else if (this.currentTool === 'eraser') {
+      this.shape = new Eraser(this.ctx, this.pointList)
+    } else if (this.currentTool === 'line') {
+      this.shape = new Line(this.ctx, start, this.generatePoint(x, y))
+    } else if (this.currentTool === 'rect') {
+      this.shape = new Rect(this.ctx, start, x - start.x, y - start.y);
     }
   }
 
-  drawEnd () {
+  drawEnd (x: number, y: number) {
+    this.isDrawing && this.shape && this.shapeList.push(this.shape);
+    this.pointList = [];
     this.isDrawing = false;
-    this.shapeList = [];
-  }
-
-  text (x: number, y: number) {
-    if (this.txtInput) {
-      this.ctx.fillText(this.txtInput.value, this.shapeList[0][0], this.shapeList[0][1]);
-      this.el.parentNode.removeChild(this.txtInput);
-      this.txtInput = null;
-      this.shapeList = [];
-    } else {
-      this.shapeList.push([x, y]);
-      this.txtInput = document.createElement('textarea');
-      this.txtInput.style.position = 'absolute';
-      this.txtInput.style.left = x + 'px';
-      this.txtInput.style.top = y + 'px';
-      this.txtInput.style.border = '1px solid #000';
-      this.el.parentNode.appendChild(this.txtInput);
-    }
-  }
-
-  rect (x: number, y: number) {
-    if (this.isDrawing) {
-      this.clear();
-      const firstShape: number[] = this.shapeList[0];
-      this.shapeList.push([x, y]);
-      this.ctx.fillRect(firstShape[0], firstShape[1], x - firstShape[0], y - firstShape[1]);
-    }
-  }
-
-  line (x: number, y: number) {
-    if (this.isDrawing) {
-      this.clear();
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.shapeList[0][0], this.shapeList[0][1]);
-      this.ctx.lineTo(x, y);
-      this.ctx.stroke();
-    }
+    this.shape = null;
+    console.log(this.shapeList);
   }
 }
