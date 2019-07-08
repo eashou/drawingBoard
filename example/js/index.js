f17e67e6c13119309419ebe9d93bb599aaad26fc
shaoyishou <shaoyishou@gmail.com>
2019-07-08 20:38:36 +0800

    add demo tool ui

--- example/js/index.js
+++ example/js/index.js
@@ -1,3 +1,24 @@
 
 var oBoard = document.getElementById('board');
 var db = new DrawingBoard(oBoard);
+
+var zoomFator = 10;
+function zoom (bOk) {
+  bOk ? zoomFator++ : zoomFator--;
+  zoomFator = zoomFator < 1 ? 1 : zoomFator >= 20 ? 20 : zoomFator;
+  db.zoom(zoomFator / 10);
+}
+
+
+var aTools = document.getElementsByTagName('li');
+for (var index = 0; index < aTools.length; index++) {
+  (function (i) {
+    var currTool = aTools[i];
+    currTool.addEventListener('click', function () {
+      for (var index = 0; index < aTools.length; index++) {
+        aTools[index].className = 'tool';
+      }
+      currTool.className = 'tool active';
+    }, false);
+  })(index);
+}
